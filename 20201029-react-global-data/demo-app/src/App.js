aa567c646839c8055958020d1548cbbce6518b71
ynonp <ynonperek@gmail.com>
2020-10-25 15:54:28 +0200

    context

--- 20201029-react-global-data/demo-app/src/App.js
+++ 20201029-react-global-data/demo-app/src/App.js
@@ -1,16 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect, useCallback, useMemo } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import _ from 'lodash';
 
+const GameContext = React.createContext({ totalScore: 0, gameIndex: 0 });
+
 function ControlPanel(props) {
-  function reset() {
-  }
+  const { reset, totalScore } = useContext(GameContext);
 
   return (
     <div className="control-panel">
       <p>
-        Total Score: {0}
+        Total Score: {totalScore}
         <button onClick={reset}>Reset Everything</button>
       </p>
     </div>
@@ -20,13 +21,21 @@ function ControlPanel(props) {
 function RedSpotter(props) {
   const [score, setScore] = useState(0);
   const [winnerIndex, setWinnerIndex] = useState(_.random(4));
+  const { addScore, gameIndex } = useContext(GameContext);
+
+  useEffect(function() {
+    setScore(0);
+    setWinnerIndex(0);
+  }, [gameIndex]);
 
   function handleClick(i) {
     if (i === winnerIndex) {
       setWinnerIndex(_.random(4));
       setScore(s => s + 5);
+      addScore(5);
     } else {
       setScore(s => s - 2);
+      addScore(-2);
     }
   }
 
@@ -45,13 +54,34 @@ function RedSpotter(props) {
 }
 
 function App() {
+  const [totalScore, setTotalScore] = useState(0);
+  const [gameIndex, setGameIndex] = useState(Math.random());
+
+  function reset() {
+    setGameIndex(Math.random());
+    setTotalScore(0);
+  }
+
+  function addScore(s) {
+    setTotalScore(score => score + s);
+  }
+
+  const globalData = {
+    totalScore,
+    gameIndex,
+    reset,
+    addScore,
+  };
+
   return (
     <div className="App">
-      <ControlPanel />
-      <RedSpotter />
-      <RedSpotter />
-      <RedSpotter />
-      <RedSpotter />
+      <GameContext.Provider value={globalData}>
+        <ControlPanel />
+        <RedSpotter />
+        <RedSpotter />
+        <RedSpotter />
+        <RedSpotter />
+      </GameContext.Provider>
     </div>
   );
 }
